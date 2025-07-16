187b58417559005eb9928441a38bb6cbf09e869c
dfine <coding@dfine.tech>
2025-07-16 13:59:16 +0800

    feat: add user_control endpoint for managing user status
    - implement user_control function to update user active status
    - update routes to include new /disiable/user endpoint

--- src/routes/control.rs
+++ src/routes/control.rs
@@ -1,13 +1,13 @@
-use crate::db::users::Entity as Users;
+use crate::db::users::{self, Entity as Users};
 use std::sync::Arc;
 
 use axum::{
     Extension, Json, Router,
-    extract::State,
-    routing::{get, post},
+    extract::{Path, State},
+    routing::{delete, get, post},
 };
 use chrono::{DateTime, Utc};
-use sea_orm::{DatabaseConnection, EntityTrait};
+use sea_orm::{ActiveValue::Set, DatabaseConnection, EntityTrait};
 use serde::{Deserialize, Serialize};
 use tower_http::validate_request::ValidateRequestHeaderLayer;
 use tracing::error;
@@ -88,6 +88,45 @@ pub async fn all_user(
     AppResponse::ok("Query users finished".to_string(), Some(res))
 }
 
+pub async fn delete_user(
+    State(data): State<Arc<AppState>>,
+    Extension(claims): Extension<TokenClaims>,
+    Path(user_id): Path<i64>,
+) -> AppResponse<String> {
+    if !is_admin(claims.id, &data.sql_conn).await {
+        return AppResponse::internal_err(format!("Only super_admin can perform this action"));
+    }
+    match Users::delete_by_id(user_id).exec(&data.sql_conn).await {
+        Ok(_) => return AppResponse::ok(format!("User {user_id} has been deleted"), None),
+        Err(e) => return AppResponse::internal_err(format!("Failed to delete users: {e}")),
+    }
+}
+
+#[derive(Deserialize)]
+pub struct UserControlInfo {
+    user_id: i64,
+    disable: bool,
+}
+
+pub async fn user_control(
+    State(data): State<Arc<AppState>>,
+    Extension(claims): Extension<TokenClaims>,
+    Json(payload): Json<UserControlInfo>,
+) -> AppResponse<String> {
+    if !is_admin(claims.id, &data.sql_conn).await {
+        return AppResponse::internal_err(format!("Only super_admin can perform this action"));
+    }
+    let invalid_user = users::ActiveModel {
+        id: Set(payload.user_id),
+        valid: Set(payload.disable as i8),
+        ..Default::default()
+    };
+    match Users::update(invalid_user).exec(&data.sql_conn).await {
+        Ok(_) => return AppResponse::ok(format!("User status has been changed"), None),
+        Err(e) => return AppResponse::internal_err(format!("Failed to delete users: {e}")),
+    }
+}
+
 pub fn routes(app_state: Arc<AppState>) -> Router {
     let jwt_auth = JwtAuth {
         conf: Arc::new(app_state.config.jwt_conf.clone()),
@@ -95,6 +134,8 @@ pub fn routes(app_state: Arc<AppState>) -> Router {
     Router::new()
         .route("/register", post(allow_register))
         .route("/list/user", get(all_user))
+        .route("/user/{user_id}", delete(delete_user))
+        .route("/disiable/user", post(user_control))
         .layer(ValidateRequestHeaderLayer::custom(jwt_auth))
         .with_state(app_state)
 }
